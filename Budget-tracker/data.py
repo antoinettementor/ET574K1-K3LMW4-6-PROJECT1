f60b27af71daed47c0bd7c949198b77d8c674393
Dimitri Davis <dimitrid233@gmail.com>
2025-10-23 21:39:29 +0000

    Updated data.py with random values

---
+++ Budget-tracker/data.py
@@ -0,0 +1,7 @@
+#Dimitri Davis
+#data.py
+#Weekly expense data for Budget Tracker project
+
+categories = ["Food", "Transportation", "Groceries", "Entertainment","Fitness", "Health", "Shopping"]
+amounts = [14.25, 35.00, 62.40, 18.00, 105.75,40.00, 58.90]
+descriptions = ["Sandwich", "Gas", "Weekly grocery trip", "Movie ticket","Gym membership", "Prescription refill", "New cloths"]
