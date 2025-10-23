0eb0beb83ac665e4ad5f276e61264721fee30263
Dimitri Davis <dimitrid233@gmail.com>
2025-10-23 21:56:19 +0000

    Updated Fitness to 25.75 for amounts- Dimitri D.

--- Budget-tracker/data.py
+++ Budget-tracker/data.py
@@ -3,5 +3,5 @@
 #Weekly expense data for Budget Tracker project
 
 categories = ["Food", "Transportation", "Groceries", "Entertainment","Fitness", "Health", "Shopping"]
-amounts = [14.25, 35.00, 62.40, 18.00, 105.75,40.00, 58.90]
+amounts = [14.25, 35.00, 62.40, 18.00, 25.75,40.00, 58.90]
 descriptions = ["Sandwich", "Gas", "Weekly grocery trip", "Movie ticket","Gym membership", "Prescription refill", "New cloths"]
